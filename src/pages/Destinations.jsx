d3f7ce064d609977da06cda22b485505669181de
ncesari49 <cesarin@hotmail.fr>
2022-11-25 11:38:21 +0100

    install lottie

--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -6,45 +6,47 @@ import "../style/Destination.css";
 import DestinationsTitle from '../components/Destinations/DestinationsTitle';
 import DestinationsBlock from '../components/Destinations/DestinationsBlock';
 import destinations from "../data";
+import Reservation from '../components/Reservation';
 // import { defaultMethod } from 'react-router-dom/dist/dom';
 
 const Destinations = () => {
 
     const [booking, setBooking] = React.useState(false);
-    
-    const clickMe =  () => {
+
+    const clickMe = () => {
         setBooking(!booking);
     }
 
 
     return (
-    <div>
-      <Header />
+        <div>
+            <Header />
 
-                <div className='titleDestination'>
-                  <DestinationsTitle />
-              </div>
-      
-          <div className='destinationForTravel'>
-              
+            <div className='titleDestination'>
+                <DestinationsTitle />
+            </div>
 
-              {destinations.map((info) => (
-                  <div>
-                      <DestinationsBlock
-                      key = {info.id}
-                      info = {info}
-                      clickMe = {clickMe}
-                      />   
-                  </div>     
-                      
-          ))}
+            <div className='destinationForTravel'>
+
+
+                {destinations.map((info) => (
+                    <div>
+                        <DestinationsBlock
+                            key={info.id}
+                            info={info}
+                            clickMe={clickMe}
+                        />
+                        <Reservation />
+                    </div>
+
+                ))}
 
 
 
             </div>
-            
-        <Footer />
-      </div>
+
+            <Footer />
+        </div>
     );
 };
 
